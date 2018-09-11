a1a1b5036c45a8ce6ae7e05f9c52f0cd0f4c6a88
Gaven Giampalmi <ggiampalmi@gavens-air.bc.edu>
2018-09-11 17:26:34 -0400

    What's For Dinner App - Initial Commit

--- What's For Dinner/ViewController.swift
+++ What's For Dinner/ViewController.swift
@@ -4,17 +4,53 @@
 //
 //  Created by Gaven Giampalmi on 9/6/18.
 //  Copyright © 2018 Gaven Giampalmi. All rights reserved.
-//
+// 
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var messageLabel: UILabel!
+    var index = -1
+    var imageIndex = -1
+    let numberOfImages = 7
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func optionButton(_ sender: UIButton) {
+        
+        let dinnerOptions = [
+            "Chicken Terryaki",
+            "Pizza",
+            "Buffalo Wings",
+            "Pasta",
+            "Sushi",
+            "Steak",
+            "Salad"]
+        
+        var newIndex: Int
+        
+        repeat {
+            newIndex = Int.random(in: 0..<dinnerOptions.count)}
+        while index == newIndex
+        
+        index = newIndex
+        messageLabel.text = dinnerOptions[index]
+        
+//        repeat {
+//            newIndex = Int.random(in: 0..<dinnerOptions.count)}
+//        while index == newIndex
+//
+//        imageIndex = newIndex
+        
+        imageView.image = UIImage(named: "image\(newIndex + 1)")
+        
+        
+    }
+    
 }
 
